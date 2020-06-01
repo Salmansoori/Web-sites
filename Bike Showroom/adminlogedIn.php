2e68835880a1fc6c317857c52ca05006f56aac6e
Md Salman Mansoori <salmansoori509@gmail.com>
2020-06-01 11:26:18 +0530

    working on

---
+++ Bike Showroom/adminlogedIn.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+     <link rel="stylesheet" href="style.css">
+
+    <title>Bike Showroom </title>
+    <style>
+    .vertical-menu {
+  width: 800px;
+}
+
+.vertical-menu a {
+  background-color:#eee;
+  color: black;
+  display: block;
+  padding: 12px;
+  text-decoration: none;
+}
+
+.vertical-menu a:hover {
+  background-color: #ccc;
+}
+
+.vertical-menu a.active {
+  background-color: red;
+  color: white;}
+.id1{
+  c
+}  
+</style>
+  </head>
+  <body>
+   <header> 
+   <h1 style="font-size: 63px;background-color: red;border: black;text-align: center; ">Bike Showroom Management System</h1> 
+   </header>
+   <div id="id1">
+   <label><font color="red">Welcome To Bike Management System</font></label>
+   <label style="margin-left:760px;font-size: 30px;"><font color="red">Advertisement</font></label>
+  </div>
+  <div class="vertical-menu">
+  <a href="sample.html"class="active">Home</a>
+  <a href="About">About Us</a>
+  <a href="#">Add customer</a>
+  <a href="#">Report Customer</a>
+  <a href="#">Add Booking</a>
+   <a href="#">Report Booking</a>
+  <a href="#">Add Company</a>
+  <a href="#">Report Company</a>
+  <a href="#">My Account</a>
+   <a href="#">Change Password</a>
+  <a href="#">Logout</a>
+  </div>
+   <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
